90de80404ba22131de3145fae81672ad5d6b268b
gera-maj <jatipg@gmail.com>
2023-05-02 00:41:31 -0300

    se colocó una nueva forma de hacer el menu

--- js/app.js
+++ js/app.js
@@ -37,3 +37,27 @@ listaNormativos.addEventListener("mouseout", () => {
 normativos.addEventListener('mouseout', ()=>{   
     listaNormativos.style.display = 'none'       
 })
+
+
+
+
+// Obtener todos los elementos del menú con submenús
+const menuItems = document.querySelectorAll("nav li");
+
+// Recorrer cada elemento del menú y agregar un event listener para el evento "mouseover"
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener("mouseover", () => {
+    const submenu = menuItem.querySelector(".submenu");
+    if (submenu) {
+      submenu.style.display = "block";
+    }
+  });
+
+  // Agregar un event listener para el evento "mouseout"
+  menuItem.addEventListener("mouseout", () => {
+    const submenu = menuItem.querySelector(".submenu");
+    if (submenu) {
+      submenu.style.display = "none";
+    }
+  });
+});
