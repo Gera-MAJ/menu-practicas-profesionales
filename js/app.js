612d73d0214c88b08a1a79b1cc02afb096b35850
gera-maj <jatipg@gmail.com>
2023-05-02 16:54:45 -0300

    modificando el menu

--- js/app.js
+++ js/app.js
@@ -1,46 +1,4 @@
 
-const practicas = document.getElementById("practicas")
-const listaPracticas = document.getElementById("listaPracticas")
-const normativos = document.getElementById("normativos")
-const listaNormativos = document.getElementById("listaNormativos")
-
-
-//con este mouseover, es cuando se pasa el mouse por encima
-practicas.addEventListener('mouseover', ()=>{  
-    listaPracticas.style.display = "block"    
-})
-
-listaPracticas.addEventListener('mouseover', () => {
-    listaPracticas.style.display = 'block'
-})
-//la característica mouseout es para cuando el dejan de pasar por encima con el mouse. 
-listaPracticas.addEventListener('mouseout', ()=>{   
-    listaPracticas.style.display = 'none'       
-})
-
-practicas.addEventListener('mouseout', ()=>{   
-    listaPracticas.style.display = 'none'       
-})
-
-normativos.addEventListener("mouseover", () => {
-    listaNormativos.style.display = 'block'
-})
-
-listaNormativos.addEventListener('mouseover', ()=> {
-    listaNormativos.style.display = 'block'
-})
-
-listaNormativos.addEventListener("mouseout", () => {
-    listaNormativos.style.display = "none"
-})
-
-normativos.addEventListener('mouseout', ()=>{   
-    listaNormativos.style.display = 'none'       
-})
-
-
-
-
 // Obtener todos los elementos del menú con submenús
 const menuItems = document.querySelectorAll("nav li");
 
