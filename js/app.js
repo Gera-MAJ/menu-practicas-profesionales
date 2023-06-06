0df89bf96abc28238784018d0c1ac8b28ecc5e24
gera-maj <jatipg@gmail.com>
2023-06-06 11:16:44 -0300

    corregido problemas de color en el submenu

--- js/app.js
+++ js/app.js
@@ -1,15 +1,13 @@
 
-// Obtener todos los elementos del menú con submenús
+// Obtener todos los elementos del menú con submenús. El queryselectorall carga todo lo que esté dentro de esa etiqueta
 const menuItems = document.querySelectorAll(".has-submenu");
-
-console.log(menuItems)
 
 // Recorrer cada elemento del menú y agregar un event listener para el evento "mouseover"
 menuItems.forEach((menuItem) => {
   menuItem.addEventListener("mouseover", () => {
     const submenu = menuItem.querySelector(".has-submenu a");
     const menu = menuItem.querySelector(".submenu")
-    console.log(submenu)
+    
     if (submenu) {
       menu.style.display = "block";
     }
